08e526c3ea3ebb0e2273961d15cc7c982f60b5c2
Themovieguy <157444825+Themovieguy@users.noreply.github.com>
2024-03-24 16:38:27 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -117,7 +117,7 @@ async def start(client, message):
             if f_caption is None:
                 f_caption = f"{title}"
             try:
-                h = await message.reply_text(f"\n\nThis Movie File/Video will be deleted in <b><u>{AUTO_DELETE} minutes</u> 🫥 <i></b>(Due to Copyright Issues)</i>.\n\n<b><i>Please forward this File/Video to your Saved Messages and Start Download there</b>")
+                h = await message.reply_text(f"\n\n⚠️ This File will be deleted in {AUTO_DELETE} minutes.\n\n<b><i>Please forward this File/Video to your Saved Messages and Start Download there</b>")
                 k = await client.send_cached_media(
                     chat_id=message.from_user.id,
                     file_id=msg.get("file_id"),
